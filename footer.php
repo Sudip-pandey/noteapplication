4015f8dd9d5d260502cb9f95234512c2990d3a1c
neeswebservices <neeswebservice@gmail.com>
2022-09-11 10:12:39 +0545

    back

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
         <footer>
-            <center> &copy; Copyright 2022 <a href="https://nischal-dahal.com.np">neeswebservices</a> | All Right
+            <center> &copy; Copyright 2022 <a href="https://nischal-dahal.com.np">neeswebservice</a> | All Right
                 Reserved</center>
         </footer>
         </body>
